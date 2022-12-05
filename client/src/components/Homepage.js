1f911ae8d8f9a598e80124d3774f83c53b65c176
Maureen Njihia <maureennjihia468@gmail.com>
2022-12-05 16:17:42 +0300

    Create and style homepage banner

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Homepage () {
+    return (
+        <section className="homepage">
+            <div className="row">
+                <div className="col-md-7">
+                    <div className="intro">
+                        <h1>Find A <span className="main-color">Job</span> That <br></br>
+                        <span className="main-color">Matches</span><br></br>Your Passion</h1>
+                        <p className="mt-4">
+                            Hand-picked opportunities to work from home, remotely, freelance, 
+                            <br></br>full-time, part-time, contract and internships.
+                        </p>
+                        <div className="search-form">
+                            <form className="search">
+                                <div className="input-group">
+                                    <input type="text" className="form-control" placeholder="Search by job title......" name="query" required></input>
+                                    <div className="input-group-append">
+                                    <button className="btn btn-primary btn-search" type="submit">
+                                        Search
+                                    </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Homepage;
