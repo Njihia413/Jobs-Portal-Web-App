eabe13716d72477229d1a5290ae9f3f92bf4ff1c
Maureen Njihia <maureennjihia468@gmail.com>
2022-12-06 08:44:16 +0300

    Add categories controller methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,43 @@
 class CategoriesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         categories = Category.all
         render json: categories
     end
 
     def show
-        category = Category.find(params[:id])
+        category = find_category
         render json: category
+    end
+
+    def create
+        category = Category.create!(category_params)
+        render json: category, status: :created
+    end
+
+    def update
+        category = find_category
+        category.update!(category_params)
+        render json: category
+    end
+
+    def destroy
+        category = find_category
+        category.destroy
+        head :no_content
+    end
+
+    private
+    def category_params
+        params.permit(:name, :description)
+    end
+
+    def find_category
+        category = Category.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Category not found" }, status: :not_found
     end
 end
