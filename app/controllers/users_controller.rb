8f3a09f6253e11141aa97e4411f18033724d51ec
Maureen Njihia <maureennjihia468@gmail.com>
2022-12-06 17:40:10 +0300

    Add user and session controller methds for signup, login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user
+    end
+
+    private
+    def user_params
+        params.permit(:username, :first_name, :last_name, :email, :password)
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
