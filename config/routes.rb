d4340fcfdaf17b13bbd40c15197e42fadc4fa2cf
Maureen Njihia <maureennjihia468@gmail.com>
2022-12-06 17:41:03 +0300

    Add signup, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   resources :categories
   resources :jobs
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/register", to: "users#create"
+  get "/me", to: "users#show"
 end
